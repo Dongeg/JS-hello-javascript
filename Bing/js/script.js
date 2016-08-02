99edfc7f7dc26b77f931a77c159664736cac3901
Dongeg <821220992@qq.com>
2016-08-02 19:10:24 +0800

    finish

--- Bing/js/script.js
+++ Bing/js/script.js
@@ -10,6 +10,44 @@ $(function(){
 	$(window).click(function(){
 		$("#seting-main").hide();
 	})
+//----------------ajax部分-------------------------------------------------	
+	$(".search-main").bind('keyup',function(){
+		//keyup实时获取用户输入值
+		var searchText=$(this).val();
+		//输入值为空直接return，否则ajax会报错
+		if(searchText=="")return;
+		//输入值转化为正则表达式的样子，只是样子
+        var searchTextZZ="/"+searchText+"/";
+        //getJSON读取json文件，他直接把json解析成了对象，不用我们手动转换了
+		$.getJSON("data.json",function(data){
+			//读取到的数据（这里就是一个数组）赋值给dataValue
+			var dataValue=data.contentValue
+			//用于存放正则表达式通过的变量
+			var valueList='';
+			for(var i=0;i<dataValue.length;i++){
+				//eval()函数进行转译，把"/"+searchText+"/"转换为正则表达式
+				if(eval(searchTextZZ).test(dataValue[i])){
+					//通过正则的变量存入valueList
+					valueList+='<li>'+dataValue[i]+'</li>';
+				}
+			}
+			//插入DOM节点
+			$("#search-result").html(valueList);
+			$(".prompt").show();
+		})
+		
+//		var searchText=$(this).val();
+//		$.get('http://api.bing.com/qsonhs.aspx?q='+searchText,function(d){
+//			var d=d.AS.Results[0].Suggests;
+//			var valueList='';
+//			for(var i=0;i<d.length;i++){
+//				valueList+='<li>'+d[i].text()+'</li>';
+//			}
+//			$("#search-result").html(valueList);
+//			$(".prompt").show();
+//		},'json');
+		
+	});
 	$(".search-main").bind('focus',function(){
 		$(".focus-bg").css('display','block');
 		setTimeout(function(){
@@ -19,12 +57,12 @@ $(function(){
 	});
 	$(".search-main").bind('blur',function(){
 		$(".focus-bg").css('opacity','0');
+		$(".prompt").hide();
 		setTimeout(function(){
 		  $(".focus-bg").css('display','none');		  			
 		},350)
 	    
 	});	
-	
 	$("#shear").bind('mouseover',function(){
 			$('.shear-content').css('opacity','1');
 			$('.shear-content').css('transform','translate(0,0)');         	
@@ -57,8 +95,8 @@ $(function(){
 			$(this).css('opacity',1);
 			$(".turn-left").css('opacity','1');
 			$('.bg-toggle').css('opacity','1');
-			setTimeout(function(){
-			$("body").css('background',carouselArray[++i]);
+			$("body").css('background-image',carouselArray[++i]);
+			setTimeout(function(){			
 			$('.bg-toggle').css('opacity','0');	
 			},500);			
 		}
@@ -73,8 +111,8 @@ $(function(){
 			$(this).css('opacity',1);
 			$(".turn-right").css('opacity','1');
 			$('.bg-toggle').css('opacity','1');
+			$("body").css('background-image',carouselArray[--i]);
 			setTimeout(function(){
-			$("body").css('background',carouselArray[--i]);
 			$('.bg-toggle').css('opacity','0');	
 			},500);			
 		}
